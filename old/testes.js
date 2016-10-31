101388d0320b6b2f4e39820b2a66d1e0277ea474
ciros <ciros@DESKTOP-FR0AO9Q>
2016-10-30 23:37:06 -0200

    Removido arquivos obsoletos

--- old/testes.js
+++
@@ -1,8 +0,0 @@
-var time = parseTime("00:00:45");
-var time2 = Time.now();
-
-
-var diff = Time.fromDateDiff(Date.now(), 0);
-console.log(diff + "");
-console.log(time + "");
-console.log(time2 + "");
