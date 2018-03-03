995797712f5ef386ea41f4c53d416bb84c7e0c8b
Ciro Spaciari <ciro.spaciari@gmail.com>
2018-03-03 11:19:48 -0300

    GLOBAL error fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-chmod a+x  build/normal.sh
-chmod a+x  build/minify.sh
-sh build/normal.sh
-sh build/minify.sh
+chmod a+x  ./build/normal.sh
+chmod a+x  ./build/minify.sh
+sh ./build/normal.sh
+sh ./build/minify.sh
--- dist/format.js
+++ dist/format.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 /*! Format Helper - 2016*/
 require("./src/Culture.js");
 require('./src/CulturesPack.js');
@@ -8,6 +8,7 @@ require("./src/Time.format.js");
 require("./src/String.format.js");
 
 },{"./src/Culture.js":2,"./src/CulturesPack.js":3,"./src/Date.format.js":4,"./src/Number.format.js":5,"./src/String.format.js":6,"./src/Time.format.js":7}],2:[function(require,module,exports){
+(function (global){
 (function (global) {
 
 	var Culture = {
@@ -39,8 +40,9 @@ require("./src/String.format.js");
 	};
 
 	global.Culture = Culture;
-})(typeof GLOBAL != "undefined" ? GLOBAL : window);
+})(typeof global != "undefined" ? global : window);
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],3:[function(require,module,exports){
 require("./Culture.js");
 
@@ -294,6 +296,7 @@ Culture.add({
 });
 
 },{"./Culture.js":2}],4:[function(require,module,exports){
+(function (global){
 require("./Culture.js");
 require("./Time.format.js");
 
@@ -690,9 +693,11 @@ require("./Time.format.js");
 
 		return newText;
 	}
-})(typeof GLOBAL != "undefined" ? GLOBAL : window);
+})(typeof global != "undefined" ? global : window);
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./Culture.js":2,"./Time.format.js":7}],5:[function(require,module,exports){
+(function (global){
 require("./Culture.js");
 (function (global) {
 
@@ -1029,8 +1034,9 @@ require("./Culture.js");
 
 	global.parseNumber = Number.parse;
 
-})(typeof GLOBAL != "undefined" ? GLOBAL : window);
+})(typeof global != "undefined" ? global : window);
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./Culture.js":2}],6:[function(require,module,exports){
 require("./Culture.js");
 require("./Number.format.js");
@@ -1076,6 +1082,7 @@ Object.defineProperty(String.prototype, 'format', {
 });
 
 },{"./Culture.js":2,"./Date.format.js":4,"./Number.format.js":5,"./Time.format.js":7}],7:[function(require,module,exports){
+(function (global){
 require("./Culture.js");
 
 (function (global) {
@@ -1456,6 +1463,7 @@ require("./Culture.js");
 
 		return newText;
 	}
-})(typeof GLOBAL != "undefined" ? GLOBAL : window);
+})(typeof global != "undefined" ? global : window);
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./Culture.js":2}]},{},[1]);
